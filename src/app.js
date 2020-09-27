fb8a0f68b612dc355510b71293cdade9a0c6eef6
Sandeep Saini <sandeepsaini@ecomexpress.in>
2020-09-27 16:43:30 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs');
 const geocode = require('./util/geocode');
 const forecast = require('./util/forecast');
 const app = express();
+const port = process.env.PORT || 3000 ;
 
 
 
@@ -87,6 +88,6 @@ app.get('*', (req, res)=>{
 })
 
 
-app.listen(3000, ()=>{
-    console.log("Server is up on port : 3000");
+app.listen(port, ()=>{
+    console.log("Server is up on port : "+port);
 });
